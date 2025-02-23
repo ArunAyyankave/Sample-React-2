7f33468ea78025fa5a264885c4747b8a524c3f4c
Arun Ayyankave <arunayk712@gmail.com>
2025-02-24 02:28:40 +0530

    CSS and netlify config

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
+import './home.css'
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <Link to="/login">Login</Link>
-      <h1>Home</h1>
+      <h1 className="home-title">Home</h1>
+      <p style={{color:"green"}}>Welcome to our website</p>
     </div>
   );
 }
